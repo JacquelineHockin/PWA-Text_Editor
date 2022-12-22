376c4b27ee6a4ae03d738460ee5cabeec6d5ee17
JacquelineHockin <jacquelinehcashman@gmail.com>
2022-12-22 15:26:10 -0800

    added plugins to weppack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,32 @@ module.exports = () => {
       path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      // does anyone know what is happening here??
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "J.A.T.E",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Jacqueline's Awesome Text Editor",
+        short_name: "J.A.T.E",
+        description: "A text editor but no one really knows why",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
